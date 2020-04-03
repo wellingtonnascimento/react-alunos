ac89210fe7c75b18a5690d0f98745674f26bdad1
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-03 17:28:25 -0300

    limpando projeto, Configurando Styled componentes

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Lorem!
+        <small>Oi </small>
+      </Title>
+      <Paragrafo>djasdhd jaodj oaid jad</Paragrafo>
+    </Container>
+  );
+}
---
+++ src/pages/Login/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: green;
+  small {
+    font-size: 12pt;
+    margin-left: 10px;
+    color: blue;
+  }
+`;
+
+export const Paragrafo = styled.p`
+  font-size: 80px;
+`;
