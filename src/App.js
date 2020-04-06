16e7b975551634e0a4f8c16ce8cd12996ce4fef8
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-06 15:23:53 -0300

    adicionando rotas pages usando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Login from './pages/Login';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './routes/index';
 
 function App() {
   return (
     <>
-      <Header />
-      <Login />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaSignOutAlt, FaUserAlt } from 'react-icons/fa';
-
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { Nav } from './styled';
 
 export default function Header() {
   return (
     <Nav>
-      <a href="">
+      <Link to="/">
         <FaHome size={24} />
-      </a>
-      <a href="">
+      </Link>
+      <Link to="/login">
         <FaUserAlt size={24} />{' '}
-      </a>
-      <a href="">
+      </Link>
+      <Link to="/sair">
         <FaSignInAlt size={24} />{' '}
-      </a>
+      </Link>
     </Nav>
   );
 }
