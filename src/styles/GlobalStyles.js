8b963ff2d959b2580560880ad401e2dd7e551e59
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-07 15:28:16 -0300

    adicionando uma nova rota e o react toastify

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import { primaryColor, primaryDarkColor } from '../config/colors';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
   *{
@@ -11,8 +12,8 @@ export default createGlobalStyle`
 
   body{
     font-family: sans-serif;
-    background: ${primaryDarkColor};
-    color: ${primaryColor};
+    background: ${colors.primaryDarkColor};
+    color: ${colors.primaryColor};
   }
 
   html,body, #root{
@@ -21,7 +22,7 @@ export default createGlobalStyle`
 
   button{
     cursor: pointer;
-    background: ${primaryColor};
+    background: ${colors.primaryColor};
     color: #fff;
     padding: 10px 20px;
     border-radius: 4px;
@@ -29,11 +30,18 @@ export default createGlobalStyle`
   }
   a{
     text-decoration: none;
-    color: ${primaryColor};
+    color: ${colors.primaryColor};
 
   }
   ul{
     list-style: none;
+  }
+  body .Toastify .Toastify__toast-container .Toastify__toast--success {
+    background: ${colors.successColor};
+  }
+
+  body .Toastify .Toastify__toast-container .Toastify__toast--error {
+    background: ${colors.errorColor};
   }
 `;
 
