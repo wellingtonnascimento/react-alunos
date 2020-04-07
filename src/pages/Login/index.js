1d9401fdc1a6b05c51f962a52412543e4947d246
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-07 15:44:24 -0300

    teste axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,18 +3,7 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
-import axios from '../../services/axios';
-
 export default function Login() {
-  React.useEffect(() => {
-    async function getData() {
-      const response = await axios.get('/alunos');
-      console.log(response);
-    }
-
-    getData();
-  }, []);
-
   return (
     <Container>
       <Title>
