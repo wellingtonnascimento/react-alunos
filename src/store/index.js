1a13780187728b4e65ff900805752c9a5b680d0c
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-14 10:47:48 -0300

    consigurando as middiwares com redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const initialState = {
-  botaoClicado: false,
-};
+import rootReduce from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'BOTAO_CLICADO': {
-      const newState = { ...state };
-      newState.botaoClicado = !newState.botaoClicado;
-      return newState;
-    }
+const sagaMiddleware = createSagaMiddleware();
 
-    default: {
-      return state;
-    }
-  }
-};
+const store = createStore(rootReduce, applyMiddleware(sagaMiddleware));
 
-const store = createStore(reducer);
+sagaMiddleware.run(rootSaga);
 
 export default store;
---
+++ src/store/modules/example/sagas.js
@@ -0,0 +1,23 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import * as actions from './actions';
+import * as types from '../types';
+
+const require = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 2000);
+  });
+
+function* exampleRequest() {
+  try {
+    yield call(require);
+    yield put(actions.clicaBotaoSuccess());
+  } catch {
+    toast.error('Deu Erro.');
+    yield put(actions.clicaBotaoFailure());
+  }
+}
+
+export default all([takeLatest(types.BOTAO_CLICADO_REQUEST, exampleRequest)]);
