db1b794b4fb58d22a2e21607009e7a4e4d4f4316
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-16 15:05:39 -0300

    login do usuario, ultizando saga e reducer.

--- src/store/modules/reduxPersist.js
+++ src/store/modules/reduxPersist.js
@@ -4,9 +4,9 @@ import { persistReducer } from 'redux-persist';
 export default (reducers) => {
   const persistedReducers = persistReducer(
     {
-      key: 'REACT-BASE',
+      key: 'CONSUMOAPI',
       storage,
-      whitelist: ['exemple'],
+      whitelist: ['auth'],
     },
     reducers
   );
