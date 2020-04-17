95009b55d987ab7e71bc4ecdacfa2f9c54df0604
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-17 14:40:11 -0300

    Editar dados do usuario

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -20,3 +20,31 @@ export function loginFailure(payload) {
     payload,
   };
 }
+
+export function registerRequest(payload) {
+  return {
+    type: types.REGISTER_REQUEST,
+    payload,
+  };
+}
+
+export function registerUpdateSuccess(payload) {
+  return {
+    type: types.REGISTER_UPDATE_SUCCESS,
+    payload,
+  };
+}
+
+export function registerCreateSuccess(payload) {
+  return {
+    type: types.REGISTER_CREATED_SUCCESS,
+    payload,
+  };
+}
+
+export function registerFailure(payload) {
+  return {
+    type: types.REGISTER_FAILURE,
+    payload,
+  };
+}
