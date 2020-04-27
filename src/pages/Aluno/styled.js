7d766a657752efd078d37c770b1382847a39dd8a
Wellington Nascimento <wellingto_07@hotmail.com>
2020-04-27 08:57:20 -0300

    Exibindo foto do aluno

--- src/pages/Aluno/styled.js
+++ src/pages/Aluno/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
 export const Form = styled.form`
   margin-top: 20px;
@@ -12,4 +13,31 @@ export const Form = styled.form`
     border-radius: 4px;
     padding: 0 10px;
   }
+`;
+
+export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+  position: relative;
+  margin-top: 20px;
+  img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    position: absolute;
+    bottom: 0;
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
 `;
